d44820c918a0e295f658f42ccfc35cb3e851356f
hridasha <hridashajoshi@gmail.com>
2025-05-05 11:56:52 +0530

    Added Render domain to ALLOWED_HOSTS

--- docai/wsgi.py
+++ docai/wsgi.py
@@ -11,6 +11,15 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+# Check if the model is installed
+try:
+    import spacy
+    nlp = spacy.load('en_core_web_sm')  # Replace with your model
+    print("Model loaded successfully!")
+except Exception as e:
+    print(f"Error loading model: {e}")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docai.settings')
 
 application = get_wsgi_application()
+
